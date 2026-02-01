35623b730a9c9c4d8eac50361f5014fa75b90a80
yoelgarcialago <yoelgarcialago@gmail.com>
2026-02-01 19:16:18 +0100

    Import innecesario eliminado (index.css)

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,7 +3,6 @@ import { Link, useLocation } from "react-router-dom"; // IMPORTANTE
 import Fuse from "fuse.js";
 import type { Review } from "../types/Reviews";
 import ThemeToggle from "./ThemeToggle";
-import "../styles/general/index.css"
 import styles from '../styles/modules/searchBar.module.css'
 
 export default function SearchBar() {
